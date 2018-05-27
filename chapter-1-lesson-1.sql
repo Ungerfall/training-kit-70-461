63f9209d9a87730449d80ea43f73beb523b31bfb
ungerfall <ungerfall@gmail.com>
2018-05-27 18:28:40 +0400

    Add completed lesson 1 excercise

---
+++ chapter-1-lesson-1.sql
@@ -0,0 +1,17 @@
+﻿-- selecting in non-relational way
+select
+	custid
+	,year(orderdate)
+from Sales.Orders
+order by 1, 2
+
+/*
+  selecting in relational way:
+    * give an alias to an attribute (column)
+    * return only unique values (what set theory requires)
+    * remove ordering (there is no ordering in sets)
+*/
+select distinct
+	custid
+	,year(orderdate) as orderyear
+from Sales.Orders
