5a2e5a456a394a6e0173fe01b7e3fcd0c1b84e5c
ungerfall <ungerfall@gmail.com>
2018-06-03 16:30:22 +0400

    Add completed lesson 1 chapter 1 excercise

---
+++ chapter-2-lesson-1.sql
@@ -0,0 +1,17 @@
+﻿/*
+  a table aliase.
+*/
+select
+  S.shipperid
+  ,companyname
+  ,phone
+from Sales.Shippers S
+
+/*
+  a column aliase.
+*/
+select
+  S.shipperid
+  ,companyname
+  ,phone AS [phone number]
+from Sales.Shippers S
