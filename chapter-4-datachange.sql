39a9ff006e37a09192563af84daab05cbae298f9
ungerfall <ungerfall@gmail.com>
2018-08-09 20:21:11 +0400

    Add datachage file from chapter-4 (adding a new supplier w/o any related products in order to demonstrate nonmatches)

---
+++ chapter-4-datachange.sql
@@ -0,0 +1,4 @@
+INSERT INTO Production.Suppliers
+(companyname, contactname, contacttitle, address, city, postalcode, country, phone)
+VALUES(N'Supplier XYZ', N'Jiru', N'Head of Security', N'42 Sekimai Musashino-shi',
+N'Tokyo', N'01759', N'Japan', N'(02) 4311-2609');
