83ada8c1d05127058ebc6e1ae0f6a5106d1b829a
ungerfall <ungerfall@gmail.com>
2018-08-13 01:13:40 +0400

    Add completed lesson 4 chapter 1 excercise

---
+++ chapter-4-lesson-1.sql
@@ -0,0 +1,32 @@
+﻿/*
+Write a query that matches customers with their respective orders, returning only
+matches. You are not required to return customers with no related orders. 
+*/
+select c.custid, c.companyname, o.orderid, o.orderdate
+from sales.customers c
+inner join sales.orders o on c.custid = o.custid;
+
+/*
+Revise your query to also include customers without orders.
+Alter the join type to a left outer join, as follows
+*/
+select c.custid, c.companyname, o.orderid, o.orderdate
+from sales.customers c
+left outer join sales.orders o on c.custid = o.custid;
+
+/*
+Return only customers without orders. 
+*/
+select c.custid, c.companyname, o.orderid, o.orderdate
+from sales.customers c
+left outer join sales.orders o on c.custid = o.custid
+where o.custid is null;
+
+/*
+Write a query that returns all customers, but match orders only if they were placed in
+February 2008.
+*/
+select c.custid, c.companyname, o.orderid, o.orderdate
+from sales.customers c
+left outer join sales.orders o on c.custid = o.custid
+where o.orderdate >= '20080201' and o.orderdate < '20080301';
