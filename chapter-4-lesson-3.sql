cefd78b764c8429bd1ffcb400efa13ce52ef76a0
ungerfall <ungerfall@gmail.com>
2018-11-14 21:55:47 +0400

    Add comleted lesson 4 chapter 3 excercise

---
+++ chapter-4-lesson-3.sql
@@ -0,0 +1,32 @@
+﻿/*
+clean up
+*/
+DELETE FROM Production.Suppliers WHERE supplierid > 29;
+IF OBJECT_ID('Sales.RankedProducts', 'V') IS NOT NULL DROP VIEW Sales.RankedProducts;
+IF OBJECT_ID('HR.GetManagers', 'IF') IS NOT NULL DROP FUNCTION HR.GetManagers;
+
+/*
+basic except
+*/
+SELECT empid
+FROM Sales.Orders
+WHERE custid = 1
+
+EXCEPT
+
+SELECT empid
+FROM Sales.Orders
+WHERE custid = 2;
+
+/*
+return employees who handled orders for both customer 1 and customer 2.
+*/
+select empid
+from Sales.Orders
+where custid = 1
+
+INTERSECT
+
+select empid
+from Sales.Orders
+where custid = 2;
