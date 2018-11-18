e4ba14e026a25ddeabfc970811d03992e2ce26a9
ungerfall <ungerfall@gmail.com>
2018-11-18 21:24:30 +0400

    Add completed lesson 5 chapter 1 excercise

---
+++ chapter-5-lesson-1.sql
@@ -0,0 +1,44 @@
+﻿/*
+group and aggregate data involving customers and orders.
+*/
+SELECT C.custid, COUNT(*) AS numorders
+FROM Sales.Customers AS C
+ INNER JOIN Sales.Orders AS O
+ ON C.custid = O.custid
+WHERE C.country = N'Spain'
+GROUP BY C.custid;
+GO
+
+/*
+Add the city information in the output of the query
+*/
+SELECT C.custid, C.city, COUNT(*) AS numorders
+FROM Sales.Customers AS C
+ INNER JOIN Sales.Orders AS O
+ ON C.custid = O.custid
+WHERE C.country = N'Spain'
+GROUP BY C.custid;
+GO
+
+/*
+ add city to the GROUP BY clause
+*/
+SELECT C.custid, C.city, COUNT(*) AS numorders
+FROM Sales.Customers AS C
+ INNER JOIN Sales.Orders AS O
+ ON C.custid = O.custid
+WHERE C.country = N'Spain'
+GROUP BY C.custid, C.city;
+GO
+
+/*
+GROUPING SETS
+*/
+SELECT C.custid, C.city, COUNT(*) AS numorders
+FROM Sales.Customers AS C
+ INNER JOIN Sales.Orders AS O
+ ON C.custid = O.custid
+WHERE C.country = N'Spain'
+GROUP BY GROUPING SETS ( (C.custid, C.city), () )
+ORDER BY GROUPING(C.custid);
+GO
